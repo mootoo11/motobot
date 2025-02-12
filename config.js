04f7b7c20fb974aa3e0229f8553541b631d43b95
mootoo11 <69162066+mootoo11@users.noreply.github.com>
2025-02-12 13:23:37 -0800

    config.js

--- config.js
+++ config.js
@@ -12,38 +12,6 @@ module.exports = {
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-    // Existing Nodes
-    {
-      name: "Koi Node V4",
-      password: process.env.NODE_PASSWORD_1,
-      host: "lavav4.prplmoe.me",
-      port: 1118,
-      secure: false
-    },
-    {
-      name: "ChalresNaig Node",
-      password: process.env.NODE_PASSWORD_2,
-      host: "lavahatry4.techbyte.host",
-      port: 3000,
-      secure: false
-    },
-
-    // Version 3.x (Unmaintained)
-    {
-      name: "AjieBlogs V3",
-      password: "https://dsc.gg/ajidevserver",
-      host: "lava-v3.ajieblogs.eu.org",
-      port: 443,
-      secure: true
-    },
-    {
-      name: "Serenetia V3",
-      password: "https://dsc.gg/ajidevserver",
-      host: "lavalinkv3-id.serenetia.com",
-      port: 443,
-      secure: true
-    },
-
     // Version 4.x
     {
       name: "AjieBlogs V4",
@@ -59,8 +27,6 @@ module.exports = {
       port: 443,
       secure: true
     },
-
-    // Version 3.x & 4.x
     {
       name: "AjieBlogs V3 & V4",
       password: "https://dsc.gg/ajidevserver",
@@ -75,8 +41,6 @@ module.exports = {
       port: 443,
       secure: true
     },
-
-    // Version 4.0.8 Hosted by Weiss Owl
     {
       name: "Alfari V4.0.8",
       password: "catfein",
@@ -84,34 +48,106 @@ module.exports = {
       port: 443,
       secure: true
     },
-
-    // Version 4.0.8 | Lavalink by Patryk
     {
-      name: "Muzykant V4.0.8",
+      name: "Private Hosting",
+      password: "kaligand18",
+      host: "18.209.210.50",
+      port: 25565,
+      secure: false
+    },
+    {
+      name: "DivaHost",
+      password: "divahostv4",
+      host: "lavalink.divahost.net",
+      port: 60002,
+      secure: false
+    },
+    {
+      name: "Muzykant V4 SSL",
       password: "https://discord.gg/v6sdrD9kPh",
       host: "lavalink_v4.muzykant.xyz",
       port: 443,
       secure: true
     },
-
-    // Version 3.7.13 | Lavalink by Patryk
     {
-      name: "Muzykant V3.7.13",
+      name: "Muzykant V4",
       password: "https://discord.gg/v6sdrD9kPh",
-      host: "lavalink_v3.muzykant.xyz",
-      port: 443,
+      host: "lavalink_v4.muzykant.xyz",
+      port: 80,
+      secure: false
+    },
+    {
+      name: "US",
+      password: "lavalink@itzrandomcloud",
+      host: "node.itzrandom.cloud",
+      port: 9000,
+      secure: false
+    },
+    {
+      name: "ApexNodes V4",
+      password: "apexnodes.xyz",
+      host: "v4.bloggertasher.ru",
+      port: 26040,
+      secure: false
+    },
+    {
+      name: "INZEWORLD.COM (DE)",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port: 3128,
+      secure: false
+    },
+    {
+      name: "RudraCloud",
+      password: "RudraCloud.com",
+      host: "lavalink.rudracloud.com",
+      port: 2333,
+      secure: false
+    },
+    {
+      name: "ChalresNaig Node",
+      password: "NAIGLAVA-dash.techbyte.host",
+      host: "lavahatry4.techbyte.host",
+      port: 3000,
+      secure: false
+    },
+    {
+      name: "ChalresNaig Node (Backup)",
+      password: "charlesnaig-lavalink",
+      host: "212.227.65.132",
+      port: 10296,
+      secure: false
+    },
+    {
+      name: "USA",
+      password: "jil@11097",
+      host: "badass.kalamii.net",
+      port: 8170,
+      secure: false
+    },
+    {
+      name: "br0kE's Lavalink V4 Node",
+      password: "br0k3lava4_server",
+      host: "lavalink.pct.lol",
+      port: 8080,
       secure: true
     },
-
-    // Version 3.7.13 (NO YT) | Lavalink by Patryk
     {
-      name: "Muzykant V3.7.13 (NO YT)",
-      password: "https://discord.gg/v6sdrD9kPh",
-      host: "lavalink_v3_no_yt.muzykant.xyz",
-      port: 443,
-      secure: true
+      name: "4Kep",
+      password: "4KepBot",
+      host: "zetzy.xyz",
+      port: 2333,
+      secure: false
+    },
+    {
+      name: "JM Lite LAVALINK",
+      password: "jmlitev4",
+      host: "46.202.82.164",
+      port: 1027,
+      secure: false
     }
   ]
 };
+
 
 
