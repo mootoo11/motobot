a41bf310de607eba908445fe483617717100ad86
mootoo11 <69162066+mootoo11@users.noreply.github.com>
2025-02-12 09:34:51 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-
-
 module.exports = {
   TOKEN: "",
   language: "en",
@@ -14,19 +12,105 @@ module.exports = {
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-     {
+    // Existing Nodes
+    {
       name: "Koi Node V4",
-      password: "prplmoe.me",
+      password: process.env.NODE_PASSWORD_1,
       host: "lavav4.prplmoe.me",
       port: 1118,
       secure: false
     },
     {
       name: "ChalresNaig Node",
-      password: "NAIGLAVA-dash.techbyte.host",
+      password: process.env.NODE_PASSWORD_2,
       host: "lavahatry4.techbyte.host",
       port: 3000,
       secure: false
+    },
+
+    // Version 3.x (Unmaintained)
+    {
+      name: "AjieBlogs V3",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v3.ajieblogs.eu.org",
+      port: 443,
+      secure: true
+    },
+    {
+      name: "Serenetia V3",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lavalinkv3-id.serenetia.com",
+      port: 443,
+      secure: true
+    },
+
+    // Version 4.x
+    {
+      name: "AjieBlogs V4",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 443,
+      secure: true
+    },
+    {
+      name: "Serenetia V4",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lavalinkv4.serenetia.com",
+      port: 443,
+      secure: true
+    },
+
+    // Version 3.x & 4.x
+    {
+      name: "AjieBlogs V3 & V4",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-all.ajieblogs.eu.org",
+      port: 443,
+      secure: true
+    },
+    {
+      name: "Serenetia V3 & V4",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lavalink.serenetia.com",
+      port: 443,
+      secure: true
+    },
+
+    // Version 4.0.8 Hosted by Weiss Owl
+    {
+      name: "Alfari V4.0.8",
+      password: "catfein",
+      host: "lavalink.alfari.id",
+      port: 443,
+      secure: true
+    },
+
+    // Version 4.0.8 | Lavalink by Patryk
+    {
+      name: "Muzykant V4.0.8",
+      password: "https://discord.gg/v6sdrD9kPh",
+      host: "lavalink_v4.muzykant.xyz",
+      port: 443,
+      secure: true
+    },
+
+    // Version 3.7.13 | Lavalink by Patryk
+    {
+      name: "Muzykant V3.7.13",
+      password: "https://discord.gg/v6sdrD9kPh",
+      host: "lavalink_v3.muzykant.xyz",
+      port: 443,
+      secure: true
+    },
+
+    // Version 3.7.13 (NO YT) | Lavalink by Patryk
+    {
+      name: "Muzykant V3.7.13 (NO YT)",
+      password: "https://discord.gg/v6sdrD9kPh",
+      host: "lavalink_v3_no_yt.muzykant.xyz",
+      port: 443,
+      secure: true
     }
   ]
-}
+};
+
