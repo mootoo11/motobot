85199798d9611b61f8fcafc2315c09bbc2a2ff8f
mootoo11 <69162066+mootoo11@users.noreply.github.com>
2025-02-12 13:41:56 -0800

    config.js

--- config.js
+++ config.js
@@ -21,89 +21,19 @@ module.exports = {
       secure: true
     },
     {
-      name: "Serenetia V4",
-      password: "https://dsc.gg/ajidevserver",
-      host: "lavalinkv4.serenetia.com",
-      port: 443,
-      secure: true
-    },
-    {
       name: "AjieBlogs V3 & V4",
       password: "https://dsc.gg/ajidevserver",
       host: "lava-all.ajieblogs.eu.org",
       port: 443,
       secure: true
-    },
-    {
-      name: "Serenetia V3 & V4",
-      password: "https://dsc.gg/ajidevserver",
-      host: "lavalink.serenetia.com",
-      port: 443,
-      secure: true
-    },
-    {
-      name: "Alfari V4.0.8",
-      password: "catfein",
-      host: "lavalink.alfari.id",
-      port: 443,
-      secure: true
-    },
-    {
-      name: "Private Hosting",
-      password: "kaligand18",
-      host: "18.209.210.50",
-      port: 25565,
-      secure: false
-    },
-    {
-      name: "DivaHost",
-      password: "divahostv4",
-      host: "lavalink.divahost.net",
-      port: 60002,
-      secure: false
-    },
-    {
-      name: "Muzykant V4 SSL",
-      password: "https://discord.gg/v6sdrD9kPh",
-      host: "lavalink_v4.muzykant.xyz",
-      port: 443,
-      secure: true
-    },
-    {
-      name: "Muzykant V4",
-      password: "https://discord.gg/v6sdrD9kPh",
-      host: "lavalink_v4.muzykant.xyz",
-      port: 80,
-      secure: false
-    },
-    {
-      name: "US",
-      password: "lavalink@itzrandomcloud",
-      host: "node.itzrandom.cloud",
-      port: 9000,
-      secure: false
-    },
-    {
-      name: "ApexNodes V4",
-      password: "apexnodes.xyz",
-      host: "v4.bloggertasher.ru",
-      port: 26040,
-      secure: false
-    },
+    }, 
     {
       name: "INZEWORLD.COM (DE)",
       password: "saher.inzeworld.com",
       host: "lava.inzeworld.com",
       port: 3128,
       secure: false
-    },
-    {
-      name: "RudraCloud",
-      password: "RudraCloud.com",
-      host: "lavalink.rudracloud.com",
-      port: 2333,
-      secure: false
-    },
+    },  
     {
       name: "ChalresNaig Node",
       password: "NAIGLAVA-dash.techbyte.host",
@@ -119,27 +49,6 @@ module.exports = {
       secure: false
     },
     {
-      name: "USA",
-      password: "jil@11097",
-      host: "badass.kalamii.net",
-      port: 8170,
-      secure: false
-    },
-    {
-      name: "br0kE's Lavalink V4 Node",
-      password: "br0k3lava4_server",
-      host: "lavalink.pct.lol",
-      port: 8080,
-      secure: true
-    },
-    {
-      name: "4Kep",
-      password: "4KepBot",
-      host: "zetzy.xyz",
-      port: 2333,
-      secure: false
-    },
-    {
       name: "JM Lite LAVALINK",
       password: "jmlitev4",
       host: "46.202.82.164",
@@ -148,6 +57,10 @@ module.exports = {
     }
   ]
 };
+
+
+
+
 
 
 
